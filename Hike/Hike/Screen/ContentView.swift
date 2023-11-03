71ab7493c3a8285accb96edf77dbd83fbbcfffc3
Javier Rodriguez <94465348+ILXavi@users.noreply.github.com>
2023-11-03 14:01:53 +0100

    Add files via upload
    Initial commit

---
+++ Hike/Hike/Screen/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Javier Eduardo Rodriguez Ardila on 9/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       CardView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
